3da5846ef8fb4afb6ac3ad7ac61b6bf852fcc30c
Mohamed fradi <Mohamed@tRppWcdwgg.local>
2021-06-23 09:14:14 +0300

    First Commit

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts/app')
+@section('content')
+
+
+   <!--MAIN BANNER AREA START -->
+<div class="banner-area banner-3">
+    <div class="overlay dark-overlay"></div>
+    <div class="d-table">
+        <div class="d-table-cell">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 m-auto text-center col-sm-12 col-md-12">
+                        <div class="banner-content content-padding">
+                            <h5 class="subtitle">BEST CRM IN THE MARKET</h5>
+                            <h1 class="banner-title">CRM WITH ESSENTIAL FEATURES</h1>
+                            <p>We provide marketing services to startups and small businesses to looking for a partner for their digital media, design-area.We are a a startup company to be commited to work and time management.</p>
+
+                            <a href="#" class="btn btn-white btn-circled">lets start</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--MAIN HEADER AREA END -->
+
+<!--  SERVICE AREA START  -->
+<section id="about" class="bg-light">
+    <div class="about-bg-img d-none d-lg-block d-md-block"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-sm-12 col-md-8">
+                <div class="about-content">
+                    <h5 class="subtitle">About us</h5>
+                    <h3>We are making beautiful <br>design layout for your business</h3>
+                    <p>We craft beautiful website layout from scratch.You need not to worry about site design and other technial issue.We provide these attractive service as a bonus.Let's have atalk together for your next project.</p>
+
+                    <ul class="about-list">
+                        <li><i class="icofont icofont-check-circled"></i> Feature 1</li>
+
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i>Feature 2
+                        </li>
+
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> Feature 3
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> Feature 4
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> Feature 5
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"></i> Feature 6
+                        </li>
+                    </ul>
+                    <button class="btn btn-sm btn-success btn-circled">Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--  SERVICE AREA END  -->
+
+
+@endsection
---
+++ resources/views/site/register.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts/app')
+@section('content')
+
+<!--  SERVICE AREA START  -->
+<section id="about" class="bg-light">
+    <div class="about-bg-img d-none d-lg-block d-md-block"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-sm-12 col-md-8">
+                <div class="about-content">
+                    <h5 class="subtitle">About us</h5>
+                    <h3>We are making beautiful <br>design layout for your business</h3>
+                    <p>We craft beautiful website layout from scratch.You need not to worry about site design and other technial issue.We provide these attractive service as a bonus.Let's have atalk together for your next project.</p>
+
+                    <ul class="about-list">
+                        <li><i class="icofont icofont-check-circled"></i> Feature 1</li>
+
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i>Feature 2
+                        </li>
+
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> Feature 3
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> Feature 4
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"> </i> Feature 5
+                        </li>
+                        <li>
+                            <i class="icofont icofont-check-circled"></i> Feature 6
+                        </li>
+                    </ul>
+                    <button class="btn btn-sm btn-success btn-circled">Register</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--  SERVICE AREA END  -->
+
+
+@endsection
